ade72d0cf850e4499dbd0f6a27df2232b6d40284
EdoBergamo <hello@amtriix.it>
2023-11-30 05:13:21 +0000

    [UPDATE] Added initial logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
+let bombe = [];
+let punteggio = 0;
+let celleScoperte = 0;
+let partitaInCorso = true;
+
+function generaBombe(numCelle) {
+  while (bombe.length < 16) {
+    const bomba = Math.floor(Math.random() * numCelle) + 1;
+    if (!bombe.includes(bomba)) {
+      bombe.push(bomba);
+    }
+  }
+}
+
+function scopriCella(numeroCella, cella, numCelle) {
+  if (!partitaInCorso) {
+    return;
+  }
+
+  if (bombe.includes(numeroCella)) {
+    cella.style.backgroundColor = 'red';
+    partitaInCorso = false;
+    console.log("Hai calpestato una bomba! Game Over.");
+    console.log(`Il tuo punteggio finale è: ${punteggio}`);
+  } else {
+    cella.style.backgroundColor = 'lightblue';
+    punteggio++;
+    celleScoperte++;
+
+    if (celleScoperte === (numCelle - bombe.length)) {
+      partitaInCorso = false;
+      console.log("Hai scoperto tutte le celle sicure! Hai vinto!");
+      console.log(`Il tuo punteggio finale è: ${punteggio}`);
+    }
+  }
+}
+
 function generaGriglia() {
   const grigliaDiv = document.getElementById('griglia');
   grigliaDiv.innerHTML = '';
@@ -23,6 +60,8 @@ function generaGriglia() {
       break;
   }
 
+  generaBombe(numCelle);
+
   for (let i = 1; i <= dimensioneGriglia; i++) {
     const riga = document.createElement('div');
     riga.classList.add('row');
@@ -35,9 +74,10 @@ function generaGriglia() {
       riga.appendChild(cella);
 
       cella.addEventListener("click", function () {
-        this.style.background = 'lightblue'
-        console.log(`hai cliccato la cella numero ${numeroCella}`)
-      })
+        if (partitaInCorso) {
+          scopriCella(numeroCella, cella, numCelle);
+        }
+      });
     }
 
     grigliaDiv.appendChild(riga);
